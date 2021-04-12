af673b8553fd04fc64bfcf5a480c893abe1e4524
nomadGeonilJang <wkdrjsdlf2@gmail.com>
2021-04-13 00:38:55 +0900

    계단오르기

---
+++ 섹션10/1. 계단오르기.ts
@@ -0,0 +1,10 @@
+function solution(n){
+    let dy = Array.from({length:n+1},()=>0);
+    dy[1] = 1;
+    dy[2] = 2;
+    for(let i = 3 ; i <= n ; i++){
+        dy[i] = dy[i-1]+dy[i-2];
+    }
+    return dy[n]
+}
+console.log(solution(7))
--- 섹션7/11. 뮤직비디오.ts
+++ 섹션7/11. 뮤직비디오.ts
@@ -1,7 +1,7 @@
 (()=>{
             
 function solution(m:number, songs:number[]){
-
+    
 }
 
 let arr=[1, 2, 3, 4, 5, 6, 7, 8, 9];
